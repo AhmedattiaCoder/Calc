4f4d1e66f957a70cb370336232ea7d2a9ef20017
AhmedCoder <AhmedattiaCoder@gmail.com>
2022-12-08 21:01:16 +0200

    created files

---
+++ design.py
@@ -0,0 +1,32 @@
+window='''
+.QMainWindow{
+    background-color:#e8e8e8;
+}
+'''
+buttons='''
+.QPushButton{
+    /*background-color:#fff;*/
+    font-size:20px;
+    border:2px solid #e8e8e8;
+    /*border-radius:10px;*/
+}
+.QPushButton:hover{
+    background-color:#a7a7a7;
+    font-size:20px;
+    border:2px solid #e8e8e8;
+    /*border-radius:10px;*/
+}
+'''
+e2='''
+.QLineEdit{
+    background-color:#e8e8e8;
+    font-size:30px;
+    border:2px solid #e8e8e8;
+    border-radius:10px;
+}
+'''
+l2='''
+.QLabel{
+    font-size:20px;
+}
+'''
